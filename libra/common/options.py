6fd42436935213f79b48a0f7f9e5b0fd848ccee6
David Shrewsbury <shrewsbury.dave@gmail.com>
2012-09-19 11:36:59 -0400

    Added support for common configuration file.

--- libra/common/options.py
+++ libra/common/options.py
@@ -13,17 +13,92 @@
 
 import argparse
 import logging
+import os.path
+import ConfigParser
+
+
+"""
+Common options parser.
+
+Options can come from either the command line or a configuration file
+in INI format. Command line options will override options from the config
+file.
+
+The following sections of the config file will be parsed:
+    [global]
+    [shortname]
+
+The [global] section can be used for options common to any program using
+this class. It is optional and does not need to be present in the file.
+
+The [shortname] section can be used for program-specific options.
+The value of 'shortname' comes from the Options.__init__() method.
+
+For example, this Options object:
+
+    options = Options('worker', 'Worker Daemon')
+
+Will read the [global] and [worker] config file sections. All other
+sections will be ignored.
+
+Boolean values in the configuration file must be given a true/false
+value.
+"""
 
 
 class Options(object):
     def __init__(self, shortname, title):
         self.title = title
-        self._parse_args(shortname)
+        self.shortname = shortname
+        self._arg_defaults = dict()
+        self._parse_args()
 
-    def _parse_args(self, shortname):
+    def _get_defaults_from_config(self):
+        """
+        Use the config file to get the defaults. This should be called
+        immediately after the option for the config file is defined, but
+        before all other options are defined.
+        """
+        args, remaining_args = self.parser.parse_known_args()
+
+        if args.config and os.path.exists(args.config):
+            config = ConfigParser.SafeConfigParser()
+            config.read([args.config])
+
+            # global section not required, so don't error
+            try:
+                global_items = config.items('global')
+            except ConfigParser.NoSectionError:
+                global_items = []
+
+            # program-specific section not required, so don't error
+            try:
+                section_items = config.items(self.shortname)
+            except ConfigParser.NoSectionError:
+                section_items = []
+
+            self._arg_defaults.update(dict(global_items + section_items))
+
+            # Convert booleans to correct type
+            for k, v in self._arg_defaults.items():
+                if v.upper() == 'FALSE':
+                    self._arg_defaults[k] = False
+                elif v.upper() == 'TRUE':
+                    self._arg_defaults[k] = True
+
+    def _parse_args(self):
         self.parser = argparse.ArgumentParser(
             description='Libra {title}'.format(title=self.title)
         )
+
+        # NOTE: Must be the first argument defined
+        self.parser.add_argument(
+            '-c', '--config', dest='config', default='/etc/libra/libra.ini',
+            metavar='FILE', help='Configuration file'
+        )
+
+        self._get_defaults_from_config()
+
         self.parser.add_argument(
             '-n', '--nodaemon', dest='nodaemon', action='store_true',
             help='do not run in daemon mode'
@@ -38,16 +113,25 @@ class Options(object):
         )
         self.parser.add_argument(
             '-p', '--pid', dest='pid',
-            default='/var/run/libra/libra_{name}.pid'.format(name=shortname),
+            default='/var/run/libra/libra_{name}.pid'.format(
+                name=self.shortname
+            ),
             help='PID file to use (ignored with --nodaemon)'
         )
         self.parser.add_argument(
             '-l', '--logfile', dest='logfile',
-            default='/var/log/libra/libra_{name}.log'.format(name=shortname),
+            default='/var/log/libra/libra_{name}.log'.format(
+                name=self.shortname
+            ),
             help='Log file to use (ignored with --nodaemon)'
         )
 
     def run(self):
+        # We have to set defaults from the config AFTER all add_argument()
+        # calls because otherwise, the default= value used in those calls
+        # would take precedence.
+        if self._arg_defaults:
+            self.parser.set_defaults(**self._arg_defaults)
         return self.parser.parse_args()
 
 
