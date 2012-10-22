374326dbb040ab9c488121b49be82ca587542003
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-10-22 16:57:13 +0100

    Add client create test to add a LB to a VIP

--- tests/test_lbaas_client.py
+++ tests/test_lbaas_client.py
@@ -8,10 +8,12 @@ from StringIO import StringIO
 from libra.client.libraapi import LibraAPI
 
 class DummyArgs(object):
+    """ Fake argparse response """
     def __init__(self):
         self.lbid = 2000
 
 class DummyCreateArgs(object):
+    """ Fake argparse response for Create function """
     def __init__(self):
         self.name = 'a-new-loadbalancer'
         self.node = ['10.1.1.1:80', '10.1.1.2:81']
@@ -20,15 +22,18 @@ class DummyCreateArgs(object):
         self.vip = None
 
 class MockLibraAPI(LibraAPI):
+    """ Used to capture data that would be sent to the API server """
     def __init__(self, username, password, tenant, auth_url, region):
         self.postdata = None
         return super(MockLibraAPI, self).__init__(username, password, tenant, auth_url, region)
     def _post(self, url, **kwargs):
+        """ Store the post data and execute as normal """
         self.postdata = kwargs['body']
         return super(MockLibraAPI, self)._post(url, **kwargs)
 
 class TestLBaaSClientLibraAPI(unittest.TestCase):
     def setUp(self):
+        """ Fake a login with token """
         self.api = MockLibraAPI('username', 'password', 'tenant', 'auth_test', 'region')
         self.api.nova.management_url = "http://example.com"
         self.api.nova.auth_token = "token"
@@ -37,6 +42,7 @@ class TestLBaaSClientLibraAPI(unittest.TestCase):
         pass
 
     def testListLb(self):
+        """ Test the table generated from the LIST function """
         fake_response = httplib2.Response({"status": '200'})
         fake_body = json.dumps({
             "loadBalancers":[
@@ -84,6 +90,7 @@ class TestLBaaSClientLibraAPI(unittest.TestCase):
                     sys.stdout = orig
 
     def testGetLb(self):
+        """ Test the table generated from the STATUS function """
         fake_response = httplib2.Response({"status": '200'})
         fake_body = json.dumps({
             "id": "2000",
@@ -139,6 +146,10 @@ class TestLBaaSClientLibraAPI(unittest.TestCase):
                     sys.stdout = orig
 
     def testDeleteFailLb(self):
+        """
+        Test a failure of a DELETE function.  We don't test a succeed yet
+        since that has no response so nothing to assert on
+        """
         fake_response = httplib2.Response({"status": '500'})
         fake_body = ''
         mock_request = mock.Mock(return_value=(fake_response, fake_body))
@@ -149,6 +160,11 @@ class TestLBaaSClientLibraAPI(unittest.TestCase):
                     self.api.delete_lb(args)
 
     def testCreateLb(self):
+        """
+        Tests the CREATE function, tests that:
+        1. We send the correct POST data
+        2. We create a table from the response correctly
+        """
         fake_response = httplib2.Response({"status": '202'})
         fake_body = json.dumps({
             'name': 'a-new-loadbalancer',
@@ -177,6 +193,7 @@ class TestLBaaSClientLibraAPI(unittest.TestCase):
                     }
                 ]
             })
+        # This is what the POST data should look like based on the args passed
         post_compare = {
                     "name": "a-new-loadbalancer",
                     "nodes": [
@@ -203,6 +220,83 @@ class TestLBaaSClientLibraAPI(unittest.TestCase):
                     self.api.create_lb(args)
                     self.assertEquals(post_compare, self.api.postdata)
                     output = out.getvalue().strip()
+                    # At some point we should possibly compare the complete
+                    # table rendering somehow instead of basic field data
+                    self.assertRegexpMatches(output, 'ROUND_ROBIN')
+                    self.assertRegexpMatches(output, 'BUILD')
+                    self.assertRegexpMatches(output, '144')
+                finally:
+                    sys.stdout = orig
+
+    def testCreateAddLb(self):
+        """
+        Tests the CREATE function as above but adding a load balancer to a
+        virtual IP
+        """
+        fake_response = httplib2.Response({"status": '202'})
+        fake_body = json.dumps({
+            'name': 'a-new-loadbalancer',
+            'id': '144',
+            'protocol': 'HTTP',
+            'port': '83',
+            'algorithm': 'ROUND_ROBIN',
+            'status': 'BUILD',
+            'created': '2011-04-13T14:18:07Z',
+            'updated': '2011-04-13T14:18:07Z',
+            'virtualIps': [
+                    {
+                        'address': '15.0.0.1',
+                        'id': '39',
+                        'type': 'PUBLIC',
+                        'ipVersion': 'IPV4',
+                    }
+                ],
+            'nodes': [
+                    {
+                        'address': '10.1.1.1',
+                        'id': '653',
+                        'port': '80',
+                        'status': 'ONLINE',
+                        'condition': 'ENABLED'
+                    }
+                ]
+            })
+        # This is what the POST data should look like based on the args passed
+        post_compare = {
+                    "name": "a-new-loadbalancer",
+                    "port": "83",
+                    "protocol": "HTTP",
+                    "virtualIps": [
+                                    {
+                                        "id": "39"
+                                    }
+                                  ],
+                    "nodes": [
+                                {
+                                    "address": "10.1.1.1",
+                                    "condition": "ENABLED",
+                                    "port": "80"
+                                }
+                             ]
+                        }
+        mock_request = mock.Mock(return_value=(fake_response, fake_body))
+        with mock.patch.object(httplib2.Http, "request", mock_request):
+            with mock.patch('time.time', mock.Mock(return_value=1234)):
+                orig = sys.stdout
+                try:
+                    out = StringIO()
+                    sys.stdout = out
+                    # Add args to add a LB to a VIP
+                    args = DummyCreateArgs()
+                    args.port = '83'
+                    args.protocol = 'HTTP'
+                    args.vip = '39'
+                    args.node = ['10.1.1.1:80']
+                    self.api.create_lb(args)
+                    self.assertEquals(post_compare, self.api.postdata)
+                    output = out.getvalue().strip()
+                    # At some point we should possibly compare the complete
+                    # table rendering somehow instead of basic field data
                     self.assertRegexpMatches(output, 'ROUND_ROBIN')
                     self.assertRegexpMatches(output, 'BUILD')
                     self.assertRegexpMatches(output, '144')
