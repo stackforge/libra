20c7c94ea4a86333640e0ca278d161d3dc37df91
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-12-06 15:26:16 +0000

    Fix Nova boot lies edge case
    Sometimes Nova can report that a 'boot' command failed when it actually did
    start a node.  This adds a check to see when this happens and adds the node
    to the failed list for re-checking.
    
    Change-Id: I5816d6562ecfdc0be4699ba148b650e9fb65a98f

--- libra/mgm/mgm.py
+++ libra/mgm/mgm.py
@@ -215,6 +215,39 @@ class Server(object):
         body['address'] = address['addr']
         return body
 
+    def find_unknown(self, name):
+        """
+            Nova can tell us a node failed to build when it didn't
+            This does a check and if it did start to build adds it to the
+            failed node list.
+        """
+        try:
+            nova = Node(
+                self.args.nova_user,
+                self.args.nova_pass,
+                self.args.nova_tenant,
+                self.args.nova_auth_url,
+                self.args.nova_region,
+                self.args.nova_keyname,
+                self.args.nova_secgroup,
+                self.args.nova_image,
+                self.args.nova_image_size,
+                node_basename=self.args.node_basename
+            )
+            node_id = nova.get_node(name)
+            self.logger.info('Storing node to try again later')
+            self.node_list.add(node_id)
+        except exceptions.NotFound:
+            # Node really didn't build
+            return
+        except exceptions.ClientException as exc:
+            # TODO: edge case where if node reports failed, actually succeeds
+            # and this node check fails we will have a dangling node
+            self.logger.error(
+                'Error getting failed node info from Nova, exception {exc}'
+                .format(exc=exc)
+            )
+
     def build_nodes(self, count, api):
         try:
             nova = Node(
@@ -244,6 +277,8 @@ class Server(object):
                 if exc.node_id > 0:
                     self.logger.info('Storing node to try again later')
                     self.node_list.add(exc.node_id)
+                else:
+                    self.find_unknown(exc.node_name)
                 self.logger.warning('Aborting node building')
                 return
             body = self.build_node_data(data)
