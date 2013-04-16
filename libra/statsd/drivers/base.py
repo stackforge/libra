0715f0a4f8ef83bd50ebeae07be53a0e9c91c516
Andrew Hutchings <andrew@linuxjedi.co.uk>
2013-04-16 13:40:23 +0100

    First push of base for statsd code
    Change-Id: Ia817023075f7041a74b402f83abb2f8324a4995a

--- libra/statsd/drivers/base.py
+++ libra/statsd/drivers/base.py
@@ -10,4 +10,15 @@
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
-# under the License.
+
+known_drivers = {
+    'dummy': 'libra.statsd.drivers.dummy.driver.DummyDriver'
+}
+
+
+class AlertDriver(object):
+    def __init__(self, logger):
+        self.logger = logger
+
+    def send_alert(self):
+        raise NotImplementedError()
