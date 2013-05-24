751143f089fa6ca4b06d018a349713289c031426
Andrew Hutchings <andrew@linuxjedi.co.uk>
2013-05-24 14:23:11 +0100

    API: add keystone and make independend app
    Now starts without pecan and uses options parser, not yet complete
    
    Change-Id: I29ef1c51d2cf71e0c60f25c3b40b1a7fdf66fe7c

--- libra/api/app.py
+++ libra/api/app.py
@@ -11,24 +11,75 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
-from pecan import make_app
+from oslo.config import cfg
+import pecan
+from libra.api import config as api_config
 from libra.api import model
+from libra.api import acl
+from wsgiref.simple_server import make_server
+from libra.common.options import Options, setup_logging
 
 
-def setup_app(config):
+auth_opts = [
+    cfg.StrOpt('auth_strategy',
+               default='keystone',
+               help='The strategy to use for auth: noauth or keystone.'),
+]
+
+CONF = cfg.CONF
+CONF.register_opts(auth_opts)
+
+
+def get_pecan_config():
+    # Set up the pecan configuration
+    filename = api_config.__file__.replace('.pyc', '.py')
+    return pecan.configuration.conf_from_file(filename)
+
+
+def setup_app(pecan_config, args):
 
     model.init_model()
 
-    return make_app(
-        config.app.root,
-        static_root=config.app.static_root,
-        template_path=config.app.template_path,
-        logging=getattr(config, 'logging', {}),
-        debug=getattr(config.app, 'debug', False),
-        force_canonical=getattr(config.app, 'force_canonical', True),
+    if not pecan_config:
+        pecan_config = get_pecan_config()
+
+    pecan.configuration.set_config(dict(pecan_config), overwrite=True)
+
+    app = pecan.make_app(
+        pecan_config.app.root,
+        static_root=pecan_config.app.static_root,
+        template_path=pecan_config.app.template_path,
+#        logging=getattr(pecan_config, 'logging', {}),
+        debug=getattr(pecan_config.app, 'debug', False),
+        force_canonical=getattr(pecan_config.app, 'force_canonical', True),
         guess_content_type_from_ext=getattr(
-            config.app,
+            pecan_config.app,
             'guess_content_type_from_ext',
             True),
     )
+
+    if not args.disable_keystone:
+        return acl.install(app, cfg.CONF)
+
+    return app
+
+def main():
+    options = Options('api', 'API Server')
+    options.parser.add_argument(
+        '--host', help='IP address to bind to, 0.0.0.0 for all IPs',
+        default='0.0.0.0'
+    )
+    options.parser.add_argument(
+        '--port', help='Port number for API server', type=int, default=8080
+    )
+    options.parser.add_argument(
+        '--disable_keystone', help='Unauthenticated server, for testing only',
+        action='store_true'
+    )
+
+    args = options.run()
+    logger = setup_logging(__name__, args)
+    pc = get_pecan_config()
+    api = setup_app(pc, args)
+    srv = make_server(args.host, args.port, api)
+    srv.serve_forever()
