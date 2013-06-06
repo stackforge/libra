ba0e40e1a556da1317d6784876d1672aee6b382e
David Shrewsbury <shrewsbury.dave@gmail.com>
2013-06-06 12:50:34 -0400

    statsd: Fix JOB_UNKNOWN import. Set min gearman.
    Change-Id: Ie92243f6c08938c8dbbc3bc4c7bf497e8b7fdc65

--- libra/statsd/gearman.py
+++ libra/statsd/gearman.py
@@ -12,7 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from gearman import JOB_UNKNOWN
+from gearman.constants import JOB_UNKNOWN
 from libra.common.json_gearman import JSONGearmanClient
 
 
