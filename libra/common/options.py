15209acdc0817d22beb41870d273d621d06b532d
David Shrewsbury <shrewsbury.dave@gmail.com>
2013-06-17 15:41:59 -0400

    Support logstash logging.
    Adds new --logstash=HOST:PORT option. All log info will be sent
    to this logstash server.
    
    Change-Id: Iddfba31ff264da609c30e1cecbd38897bb1e2c3d

--- libra/common/options.py
+++ libra/common/options.py
@@ -14,6 +14,7 @@
 import argparse
 import logging
 import logging.handlers
+import logstash
 import os
 import os.path
 import sys
@@ -160,6 +161,11 @@ class Options(object):
             help='log file to use (ignored with --nodaemon)'
         )
         self.parser.add_argument(
+            '--logstash', dest='logstash',
+            metavar="HOST:PORT", default='localhost:12345',
+            help='send logs to logstash at "host:port"'
+        )
+        self.parser.add_argument(
             '--user', dest='user',
             help='user to use for daemon mode'
         )
@@ -208,6 +214,10 @@ def setup_logging(name, args):
         handler = logging.handlers.SysLogHandler(address=args.syslog_socket,
                                                  facility=args.syslog_facility)
         handler.setFormatter(simple_formatter)
+    elif args.logstash:
+        logstash_host, logstash_port = args.logstash.split(':')
+        handler = logstash.LogstashHandler(logstash_host, logstash_port)
+        handler.setFormatter(ts_formatter)
     elif logfile:
         handler = CompressedTimedRotatingFileHandler(
             logfile, when='D', interval=1, backupCount=7
