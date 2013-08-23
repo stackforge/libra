9ea32d1dffd59159de51bd9c62933458e5900e6a
Endre Karlson <endre.karlson@gmail.com>
2013-08-23 22:50:52 +0200

    Introduce stevedore and switch to using entrypoints
    Change-Id: I3ce7e3ff5fb5d16a41910e1569d3d7d02cc8f68d

--- libra/common/utils.py
+++ libra/common/utils.py
@@ -10,4 +10,12 @@
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
-# under the License.
+from stevedore.extension import ExtensionManager
+
+
+def get_namespace_names(namespace):
+    """
+    Helper utility to get the names of the entrypoints in a namespace
+    """
+    em = ExtensionManager(namespace)
+    return em.names()
--- libra/mgm/schedulers/__init__.py
+++ libra/mgm/schedulers/__init__.py
@@ -11,9 +11,3 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
-known_modules = {
-    'build': 'libra.mgm.schedulers.build.BuildNodes',
-    'submit': 'libra.mgm.schedulers.submit.SubmitNodes'
-}
-modules = ['build', 'submit']
