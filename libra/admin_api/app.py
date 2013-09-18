270b7f59de4b7150a4da11415d937faadad56c8a
Andrew Hutchings <andrew@linuxjedi.co.uk>
2013-09-18 10:48:02 +0100

    [ADMIN_API]: Remove eventlet monkey_patch
    The eventlet monkey_patch is causing the threading timers to execute
    sequentially, locking each other out rather than in-(psuedo)parallel.
    We don't actually need it at the moment.
    
    Change-Id: I97aef29976f21c5c32ef2a299d799987aa024745

--- libra/admin_api/app.py
+++ libra/admin_api/app.py
@@ -12,7 +12,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 import eventlet
-eventlet.monkey_patch()
 import daemon
 import daemon.pidfile
 import daemon.runner
