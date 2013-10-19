7dcfd773e1df3a6ae95701b7bb0ca7dd668e8b0c
Andrew Hutchings <andrew@linuxjedi.co.uk>
2013-10-19 20:16:29 +0100

    [ADMIN_API]: Show how many devices fail on abort
    Otherwise we just show 0, 0 when things really did fail
    
    Change-Id: Ia072c8459f30a188a2f597444f109196047a1898

--- libra/admin_api/stats/scheduler.py
+++ libra/admin_api/stats/scheduler.py
@@ -108,7 +108,7 @@ class Stats(object):
                     'Too many simultaneous Load Balancer Failures.'
                     ' Aborting recovery attempt'
                 )
-                return (0, 0)
+                return pings, failed
 
             if failed > 0:
                 self._send_fails(failed_lbs)
@@ -144,7 +144,7 @@ class Stats(object):
                     'Too many simultaneous Load Balancer Failures.'
                     ' Aborting deletion attempt'
                 )
-                return (0, 0)
+                return tested, failed
 
             if failed > 0:
                 self._send_delete(failed_lbs)
